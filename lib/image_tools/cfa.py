cc3fbe186c23dd90130cc71c87e4d80060bb69f6
shungo555 <shungo.0923@gmail.com>
2020-05-25 16:53:14 +0900

    ipdate

--- lib/image_tools/cfa.py
+++ lib/image_tools/cfa.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def cfa_bayer(psize, offset = [0, 0]):
+def cfa_bayer(psize, offset = [0, 0], wide_color='g'):
     """ get CFA pattern (bayer)
     
     Parameters
@@ -10,10 +10,11 @@ def cfa_bayer(psize, offset = [0, 0]):
         [px, py]
     offset : list, optional
         [offset_x, offset_y], by default [0, 0]
-
+    wide_color : string, optional
+        bayer wide color, by default 'g'
     Returns
     -------
-    CF : array(1, px, py, rgb bands)
+    CF : array(1, px, py, filter_bands)
         mask of bayer (0 or 1)
     """
     px_s = psize[0]
@@ -32,12 +33,28 @@ def cfa_bayer(psize, offset = [0, 0]):
             
     for x in range(px_s):
         for y in range(py_s):
-            if (x % 2 != px and y % 2 != py):
-                RF[x, y] = 1
-            if (x % 2 == px and y % 2 == py):
-                BF[x, y] = 1
-            if ((x + y) % 2 != (px ^ py)):
-                GF[x, y] = 1
+
+            if wide_color=='r':
+                if (x % 2 != px and y % 2 != py):
+                    GF[x, y] = 1
+                if (x % 2 == px and y % 2 == py):
+                    BF[x, y] = 1
+                if ((x + y) % 2 != (px ^ py)):
+                    RF[x, y] = 1
+            elif wide_color=='b':
+                if (x % 2 != px and y % 2 != py):
+                    GF[x, y] = 1
+                if (x % 2 == px and y % 2 == py):
+                    RF[x, y] = 1
+                if ((x + y) % 2 != (px ^ py)):
+                    BF[x, y] = 1
+            else:
+                if (x % 2 != px and y % 2 != py):
+                    RF[x, y] = 1
+                if (x % 2 == px and y % 2 == py):
+                    BF[x, y] = 1
+                if ((x + y) % 2 != (px ^ py)):
+                    GF[x, y] = 1
 
     CF = np.zeros((1, px_s, py_s, 3))
     CF[0, :, :, 0] = RF
@@ -45,3 +62,12 @@ def cfa_bayer(psize, offset = [0, 0]):
     CF[0, :, :, 2] = BF
     
     return CF
+
+if __name__ == "__main__":
+    f = cfa_bayer([4,4], offset = [0, 0], wide_color='r')
+    print('r')
+    print(f[0,:,:,0])
+    print('g')
+    print(f[0,:,:,1])
+    print('b')
+    print(f[0,:,:,2])
--- src/weight_plot.py
+++ src/weight_plot.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import ArtistAnimation
 import argparse
 import csv
+import os,sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from lib.train_tools.plot_tool import smoothness
 
 def main(args):
     weight_array = np.load(args.input + 'sens_log.npy')
@@ -17,13 +21,13 @@ def main(args):
     print(wave_length.shape)
     for i in range(weight_array.shape[0]):
         im = ax.plot(wave_length, weight_array[i, :, 0],'r', wave_length, weight_array[i, :, 1],'g', wave_length, weight_array[i, :, 2],'b')
-        title = ax.text(0.5, 1.01, 'epoch={}, val_cpsnr={:.2f}'.format(data[i+1,1], float(data[i+1,6])),
+        title = ax.text(0.5, 1.01, 'epoch={}, val_cpsnr={:.2f}, sum={:.2f}, smoothness={:.2f}'.format(data[i+1,1], float(data[i+1,6]), np.sum(weight_array[i, :, :]), smoothness(weight_array[i,:,:])),
                     ha='center', va='bottom',
                     transform=ax.transAxes, fontsize='large')
         artists.append(im + [title])
     anim = ArtistAnimation(fig, artists, interval=100)
     # fig.show()
-    fig.suptitle('noise 0')
+    fig.suptitle('noise' + args.noise)
     anim.save(args.output + 'anim.gif', writer='imagemagick', fps=10)
     anim.save(args.output + 'anim.mp4', writer="ffmpeg")
 
